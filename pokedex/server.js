8ee87967f910dc999404c3d39954d1d5af63072d
ASIM SAMI M ALLUHAYBI <eng.asimsami@gmail.com>
2020-12-24 03:25:24 +0300

    Server is working & Done of show page through the url id & style's the page

--- pokedex/server.js
+++ pokedex/server.js
@@ -1,6 +1,6 @@
 const express    = require('express');
 const app        = express();
-const Pokemon    = require('./models/pokemon.js');
+const pokemon    = require('./models/pokemon.js');
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
@@ -9,8 +9,14 @@ app.use(express.static(__dirname + '/public'));
 // INDEX
 app.get('/', (req, res) => {
 
-    res.render('index.ejs',  { data: Pokemon });
+    res.render('index.ejs',  { data: pokemon });
 });
+
+// SHOW 
+app.get('/:id', (req, res) => {
+    let id = req.params.id;
+    res.render('show.ejs', { data: pokemon[id] });
+    });
 
 
 app.listen(4000, ()=> console.log('server is running'))
